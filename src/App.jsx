cc3e6952ef81f0a91367aaa547edcafbf404a5d4
Hudiergue <rm95395@estudante.fieb.edu.br>
2025-09-19 07:47:55 -0300

    Componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Produtos from "./pages/Produtos"
+
 const App = () => {
 
   return (
     <>
     <h1>Projeto Pizzaria </h1>
-    </>
+    <Produtos/>
+    <Produtos/>
+    <Produtos/>
+</>
   )
 }
 
---
+++ src/pages/Produtos/index.jsx
@@ -0,0 +1,15 @@
+const Produtos = () => {
+    return(
+        <div>
+            <ul>
+                <li>Pizza De Muçarela</li>
+                <li>Pizza De Calabresa</li>
+                <li>Pizza Portuguesa</li>
+                <li>Pizza Baiana</li>
+
+            </ul>
+        </div>
+    )
+}
+
+export default Produtos
