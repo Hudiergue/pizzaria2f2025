f665cf019f79fc6b5c2384f7a40c6e7f073925aa
Hudiergue <rm95395@estudante.fieb.edu.br>
2025-09-19 08:13:17 -0300

    Lista

--- src/pages/Produtos/index.jsx
+++ src/pages/Produtos/index.jsx
@@ -1,11 +1,22 @@
 const Produtos = () => {
+    
+    //Objeto com a lista de pizzas 
+
+    const pizzas = 
+    
+    'Pizza de Muçarela',
+    'Pizza de Calabresa',
+    'Pizza Baiana',
+    
+    
+    //Alteração da lista de pizzas 
+    const listaPizzas = pizzas.localeCompare(pizza=><li>{pizza<}</li>)
+    
+    
     return(
         <div>
             <ul>
-                <li>Pizza De Muçarela</li>
-                <li>Pizza De Calabresa</li>
-                <li>Pizza Portuguesa</li>
-                <li>Pizza Baiana</li>
+              {listaPizzas}
 
             </ul>
         </div>
